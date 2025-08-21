9c23833f78dd9aa6b06ff9a66f3c4a991c64a4fc
zeyadtawfek21@gmail.com <your-email@example.com>
2025-08-21 18:46:04 +0300

    add index.js in repo

---
+++ index.js
@@ -0,0 +1,15 @@
+import  express from 'express'
+import bootstrap from './src/app.controller.js'
+const app = express()
+const port = 3000
+
+
+
+bootstrap(app,express)
+
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
